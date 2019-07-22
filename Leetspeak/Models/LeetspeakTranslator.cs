edfdfc3aaa3bcf6fd73843185d1c3f0d3618d46a
Aaron Whang and Na Hyung Choi <na@>
2019-07-22 11:11:21 -0700

    clean up whitespaces and rename word variable

--- Leetspeak/Models/LeetspeakTranslator.cs
+++ Leetspeak/Models/LeetspeakTranslator.cs
@@ -4,31 +4,30 @@ namespace Leetspeak.Models
 {
     public class LeetspeakTranslator
     {
-        public string Translate(string word)
+        public string Translate(string phrase)
         {
-            string[] wordArr = word.Split(' ');
+            string[] wordArr = phrase.Split(' ');
             for (int i = 0; i < wordArr.Length; i++)
-                {  
-                    char[] charArr = wordArr[i].ToCharArray();
-                    for (int j = 1; j < charArr.Length; j++)
+            {  
+                char[] charArr = wordArr[i].ToCharArray();
+                for (int j = 1; j < charArr.Length; j++)
+                {
+                    if (charArr[j] == 's') 
                     {
-                        if (charArr[j] == 's') 
-                        {
-                            charArr[j] = 'z';
-                        }
-                    wordArr[i] = string.Join("", charArr);
+                        charArr[j] = 'z';
                     }
+                wordArr[i] = string.Join("", charArr);
                 }
-            word = string.Join(" ", wordArr);
-            return word
-                .Replace('E', 'e')
-                .Replace ('e', '3')
+            }
+            phrase = string.Join(" ", wordArr);
+            return phrase
+                .Replace('E','e')
+                .Replace('e','3')
                 .Replace('O','o')
                 .Replace('o','0')
                 .Replace('T','t')
                 .Replace('t','7')
                 .Replace('L','1');
-            
         }
     }
 }
